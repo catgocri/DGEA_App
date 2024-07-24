470a2b87325ff6b641bb2c1d432c103238213e63
catgocri <91496885+catgocri@users.noreply.github.com>
2024-07-24 11:27:41 +0200

    made renaming

--- sheetMaker2.sh
+++ sheetMaker2.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-### How to use:
-## First  argument  is the filepath to use for the searching of .fastq.gz files to create the sheet
-## Second argument  is the name of the .csv file that will be created (e.g "sheet").
-## Third argument is the output directory.
-## Example command: './sheetMaker.sh /Volumes/KINGSTON/P28103/P28103 sheetP28103 /Volumes/KINGSTON/P28103/P28103'
-##                      ^^ Will create a file in the same directory as 'sheetMaker.sh' called 'sheetP28103.csv' with the .fastq.gz files from the directory /Volumes/KINGSTON/P28103/P28103.
-
-
 # Open output directory.
 cd $3/sheetMaker
 
-#Create csv file and add header.
+# Create csv file and add header.
 touch "$2".csv
 echo "sample,fastq_1,fastq_2,strandedness" > "$2".csv
 
@@ -22,8 +14,16 @@ cd $1
 declare -A fastq_1_files
 declare -A fastq_2_files
 
-# Find all files in the given filepath that are of filetype .fastq.gz and loop through each in a sorted order.
+
+# Rename all files to end with fq.fz
 find "$1" -type f -regex ".*\.unclassified\.gz" | sort | while read -r file; do
+    # Rename the file.
+    newname=$(echo $file | sed 's/\(.*\)\.fq\.unclassified\.gz/\1.unclassified.fq.gz/')
+    mv $file $newname
+done
+
+# Find all files in the given filepath that are of filetype .unclassified.fq.gz and loop through each in a sorted order.
+find "$1" -type f -regex ".*\.unclassified\.fq\.gz" | sort | while read -r file; do
 
     # Extract the patient sample identifier and whether reverse or not (5' to 3' or 3' to 5') by cutting.
     sample=$(basename $file | cut -d'_' -f1-4)
@@ -38,10 +38,12 @@ find "$1" -type f -regex ".*\.unclassified\.gz" | sort | while read -r file; do
 
     # Wait until there exists an entry for both filepaths in the dictionaries before echoing to the csv. Then clear the dictionaries.
     if [ -n "${fastq_1_files[$sample]}" ] && [ -n "${fastq_2_files[$sample]}" ]; then
-            echo "${sample},${fastq_1_files[$sample]},${fastq_2_files[$sample]},unstranded" >> "$3"/sheetMaker/"$2".csv
-            fastq_1_files["$sample"]=""
-            fastq_2_files["$sample"]=""
+        echo "${sample},${fastq_1_files[$sample]},${fastq_2_files[$sample]},unstranded" >> "$3"/sheetMaker/"$2".csv
+        fastq_1_files["$sample"]=""
+        fastq_2_files["$sample"]=""
     fi
+
 done
 
 echo "$3"/sheetMaker/"$2".csv
+
